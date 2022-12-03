08d297d9ade7dcbd96ac95b0fdcc4c53f30e8e4a
Yaroslav Pereyaslov <raralux@yandex.ru>
2022-12-03 22:57:58 +0300

    added search track list

---
+++ app/src/main/java/com/practicum/playlistmaker/data/SearchAdapter.kt
@@ -0,0 +1,23 @@
+package com.practicum.playlistmaker.data
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.practicum.playlistmaker.R
+
+class SearchAdapter(
+    private val tracks: List<Track>
+) : RecyclerView.Adapter<SearchViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): SearchViewHolder {
+        val layout = LayoutInflater.from(parent.context).inflate(R.layout.track_view, parent, false)
+        return SearchViewHolder(layout)
+    }
+
+    override fun onBindViewHolder(holder: SearchViewHolder, position: Int) {
+        holder.bind(tracks[position])
+    }
+
+    override fun getItemCount(): Int {
+        return tracks.size
+    }
+}
---
+++ app/src/main/java/com/practicum/playlistmaker/data/Track.kt
@@ -0,0 +1,8 @@
+package com.practicum.playlistmaker.data
+
+data class Track(
+    val trackName: String,
+    val artistName: String,
+    val trackTime: String,
+    val artworkUrl100: String
+)
